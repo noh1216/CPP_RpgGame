aaf98c5959c3192d30e0e112443fe0a2f55576c0
noh1216 <noh12161216@gmail.com>
2021-12-25 14:59:52 +0900

    init

---
+++ GpgGameProject/GpgGameProject/screenPrint.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <iostream>
+#include<Windows.h>
+
+using namespace std;
+
+void gotoXY(int x, int y);
+void printMenu();
+
+// function define
+void gotoXY(int x, int y) { // change console output position
+	COORD pos;
+	pos.X = x;
+	pos.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+int printMenu() {
+	system("cls");
+	int input;
+	gotoXY(3, 10);
+	cout << "1 - start adventure";
+	gotoXY(3, 15);
+	cout << "2 - loading data";
+	gotoXY(3, 20);
+	cout << "3 - menual";
+	gotoXY(0, 49);
+	cout << "input : ";
+	cin >> input;
+	return input;
+}
